7fc8c2fdc742cea2b75925bcbffe939c134c245c
lim777 <54060243+lim777@users.noreply.github.com>
2020-08-25 17:11:30 +0700

    create migration_file staff,postion,salary to salary_projec

---
+++ database/migrations/2020_08_25_082247_create_salaries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->decimal('baseSalary',10,2);
+            $table->decimal('bonuses',7,2);
+            $table->
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salaries');
+    }
+}
